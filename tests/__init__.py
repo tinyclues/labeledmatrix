a053ec9bb2ed0f1cad8df93542c64018610afcad
Artem Kozhevnikov <artem@tinyclues.com>
2016-11-08 20:58:05 +0100

    fixing tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -5,9 +5,9 @@ import doctest
 class testDoctest(unittest.TestCase):
 
     def test_indexed_list(self):
-        import indexing.indexed_list
-        self.assertEqual(doctest.testmod(indexing.indexed_list).failed, 0)
+        import perf.indexing.indexed_list
+        self.assertEqual(doctest.testmod(perf.indexing.indexed_list).failed, 0)
 
-    # def test_karma_sparse(self):
-    #     import matrix.karma_sparse
-    #     self.assertEqual(doctest.testmod(matrix.karma_sparse).failed, 0)
+    def test_karma_sparse(self):
+        import perf.matrix.karma_sparse
+        self.assertEqual(doctest.testmod(perf.matrix.karma_sparse).failed, 0)
