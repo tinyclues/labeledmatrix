e39cd9382e0197d3b3fc260e812f0426e6520510
Artem Kozhevnikov <artem@tinyclues.com>
2017-11-23 14:10:08 +0100

    minor perf improvement for hash on exceptional class (#4387)

--- cyperf/tools/types.pyx
+++ cyperf/tools/types.pyx
@@ -13,3 +13,22 @@ cdef DTYPE_t INF = np.inf
 def set_open_mp_num_thread(int n):
     omp_set_num_threads(max(n, 1))
 
+
+cdef class FastHash:
+        """
+            fast support custom hash
+            can be used for Exceptional values classes
+             - about 4x faster than pure Python code
+        """
+        cdef long _hash
+
+        def __init__(self):
+            """
+            >>> fh = FastHash()
+            >>> hash(fh)
+            557541288731312565
+            """
+            self._hash = hash(self.__class__.__name__) + 111111
+
+        def __hash__(self):
+            return self._hash
