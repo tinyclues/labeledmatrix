f1b7e20edecc73a957cba96529ced538ff13b49e
Marc-Antoine Giuliani <marc-antoine.giuliani@tinyclues.com>
2018-02-19 14:38:33 +0100

    Exposes convergence dict of lbfgs solver in meta of bayesian_logistic… (#5702)
    * Exposes convergence dict of lbfgs solver in meta of bayesian_logistic_regression and logistic_regression libs

--- karma-core/karma/learning/logistic.py
+++ karma-core/karma/learning/logistic.py
@@ -9,6 +9,7 @@ from karma.learning.matrix_utils import diagonal_of_inverse_symposdef
 from karma.learning.regression import regression_on_blocks
 from karma.learning.utils import VirtualHStack
 from karma.thread_setter import blas_threads, open_mp_threads
+from karma.runtime import KarmaSetup
 
 
 __all__ = ['logistic_coefficients']
@@ -110,19 +111,24 @@ def logistic_coefficients_lbfgs(X, y, max_iter, C=1e10, w_warm=None, sample_weig
         w_warm = np.zeros(X.shape[1] + 1, dtype=np.float)
 
     try:
-        w0, _, _ = fmin_l_bfgs_b(logistic_loss_and_grad, w_warm, fprime=None,
-                                 args=(X, 2. * y.astype(bool) - 1, 1. / C, sample_weight),
-                                 iprint=0, pgtol=1e-7, maxiter=max_iter)
+        w0, obj_value, conv_dict = fmin_l_bfgs_b(logistic_loss_and_grad, w_warm, fprime=None,
+                                                 args=(X, 2. * y.astype(bool) - 1, 1. / C, sample_weight),
+                                                 iprint=0, pgtol=1e-7, maxiter=max_iter)
+        conv_dict['objective_value'] = obj_value
         intercept, beta = w0[-1], w0[:-1]
         linear_pred = X.dot(beta)
         linear_pred += intercept
         betas = X.split_by_dims(beta)
+
+        if KarmaSetup.verbose:
+            print(conv_dict)
+
     except Exception as ee:
         X._close_pool()  # avoid memory leak
         raise ee
 
     X._close_pool()  # Warning should called manually at the exit from class
-    return linear_pred, intercept, betas
+    return linear_pred, intercept, betas, conv_dict
 
 
 @regression_on_blocks
@@ -137,7 +143,7 @@ def logistic_coefficients_fall_back(X, y, max_iter, solver='liblinear', C=1e10, 
     lr = LogisticRegression(C=C, tol=1e-5, solver=solver, fit_intercept=True, max_iter=max_iter,
                             random_state=X.shape[0])
     lr.fit(X, y, sample_weight=sample_weight)
-    return expit(lr.decision_function(X)), lr.intercept_[0], lr.coef_[0]
+    return expit(lr.decision_function(X)), lr.intercept_[0], lr.coef_[0], {}
 
 
 def logistic_coefficients(X, y, max_iter, solver='liblinear', C=1e10,
@@ -146,11 +152,13 @@ def logistic_coefficients(X, y, max_iter, solver='liblinear', C=1e10,
         return logistic_coefficients_fall_back(X, y, max_iter, solver,
                                                C=C, sample_weight=sample_weight)
     else:
-        linear_pred, intercept, beta = logistic_coefficients_lbfgs(X, y, max_iter=max_iter, C=C,
-                                                                   w_warm=w_warm,
-                                                                   sample_weight=sample_weight,
-                                                                   nb_threads=nb_threads)
-        return expit(linear_pred), intercept, beta
+        linear_pred, intercept, beta, conv_dict = logistic_coefficients_lbfgs(X, y, max_iter=max_iter, C=C,
+                                                                              w_warm=w_warm,
+                                                                              sample_weight=sample_weight,
+                                                                              nb_threads=nb_threads)
+
+        return expit(linear_pred), intercept, beta, conv_dict
+
 
 
 def logistic_coefficients_and_posteriori(X, y, max_iter, w_priori=None, intercept_priori=0.,
@@ -178,11 +186,12 @@ def logistic_coefficients_and_posteriori(X, y, max_iter, w_priori=None, intercep
         y = 2. * y.astype(bool) - 1
     try:
         with timer('BayLogReg_Reg_Mean'):
-            w0, _, _ = fmin_l_bfgs_b(logistic_loss_and_grad, w_warm, fprime=None,
-                                     args=(X, y, alpha_priori, sample_weight, w_priori,
-                                           alpha_intercept, intercept_priori),
-                                     iprint=0, pgtol=1e-7, maxiter=max_iter)
+            w0, obj_value, conv_dict = fmin_l_bfgs_b(logistic_loss_and_grad, w_warm, fprime=None,
+                                                     args=(X, y, alpha_priori, sample_weight, w_priori,
+                                                           alpha_intercept, intercept_priori),
+                                                     iprint=0, pgtol=1e-7, maxiter=max_iter)
             intercept, beta = w0[-1], w0[:-1]
+            conv_dict['objective_value'] = obj_value
 
         with timer('BayLogReg_Reg_Variance'):
             if full_hessian:
@@ -204,7 +213,7 @@ def logistic_coefficients_and_posteriori(X, y, max_iter, w_priori=None, intercep
     X._close_pool()  # Warning should called manually at the exit from class
     return (expit(linear_pred),
             intercept, betas,
-            intercept_C_post, feature_C_posts)
+            intercept_C_post, feature_C_posts, conv_dict)
 
 
 def hessian(w, X, y, alpha, sample_weight=None, alpha_intercept=0.):
