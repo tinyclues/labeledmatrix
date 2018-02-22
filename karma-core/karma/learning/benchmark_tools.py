c9e55e8089de2308b5bd0524c11de1d1cc5f7cbe
Marc-Antoine Giuliani <marc-antoine.giuliani@tinyclues.com>
2018-02-22 09:42:24 +0100

    fixed integer division in descriptive stats (#5856)
    * fixed integer division
    
    * used mean as aggregator for proportion of non-empty rows

--- karma-core/karma/learning/benchmark_tools.py
+++ karma-core/karma/learning/benchmark_tools.py
@@ -51,8 +51,8 @@ def descriptive_features_benchmark(df, features=None, func_list=None, column_ord
     ---------------------------------------------------------------------------------------------------
     feature | is_sparse | dimension | proportion_of_nonempty_rows | m_l2   | is_positive | mean_l0_norm
     ---------------------------------------------------------------------------------------------------
-    col_2     False       2           1                             1.4748   False         2.0
-    col_1     False       2           1                             1.4142   True          2.0
+    col_2     False       2           1.0                           1.4748   False         2.0
+    col_1     False       2           1.0                           1.4142   True          2.0
     >>> with use_seed(42):
     ...    df = DataFrame({"col_1": np.ones((5, 2)), "col_2": np.random.randn(5, 2), "col_3": np.random.randn(5)})
     >>> f_list = [('mean', 'l2_norm', 'm_l2'), coherence, safe_min]
@@ -60,9 +60,9 @@ def descriptive_features_benchmark(df, features=None, func_list=None, column_ord
     ---------------------------------------------------------------------------------------------
     feature | is_sparse | dimension | proportion_of_nonempty_rows | m_l2   | coherence | safe_min
     ---------------------------------------------------------------------------------------------
-    col_2     False       2           1                             0.995    0.5752      -0.4695
-    col_3     False       1           1                             0.9619   None        -1.9133
-    col_1     False       2           1                             1.4142   1.0         1.0
+    col_2     False       2           1.0                           0.995    0.5752      -0.4695
+    col_3     False       1           1.0                           0.9619   None        -1.9133
+    col_1     False       2           1.0                           1.4142   1.0         1.0
     """
     if features is None:
         features = df.vectorial_column_names + df.sparse_column_names
@@ -72,7 +72,7 @@ def descriptive_features_benchmark(df, features=None, func_list=None, column_ord
     result_lists["feature"] = features
     result_lists["is_sparse"] = [df[feature].is_sparse() for feature in features]
     result_lists["dimension"] = [df[feat].safe_dim() for feat in features]
-    result_lists["proportion_of_nonempty_rows"] = [df.aggregate('sum(is_non_empty({}))'.format(feature)) / len(df)
+    result_lists["proportion_of_nonempty_rows"] = [df.aggregate('mean(is_non_empty({}))'.format(feature))
                                                    for feature in features]
 
     if column_order is None:
