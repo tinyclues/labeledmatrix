73c1f013983560ba95550aecdb95a7bd6818ceca
Tanguy Compagnon <tanguycs@gmail.com>
2019-04-11 18:02:06 +0200

    [KARMA EXT] move second batch of libs (#12019)
    * KARMA: dereference anomaly
    
    * [KARMA] delete lightfm from requirements
    
    * [KARMA] feature hasher w/ minimal collision back into init.py
    
    * [KARMA] cleaning of __init

--- karma-core/karma/learning/regression.py
+++ karma-core/karma/learning/regression.py
@@ -4,17 +4,13 @@
 from functools import wraps
 
 import numpy as np
-from lightfm import LightFM
 from sklearn.linear_model import Ridge
-
-from cyperf.matrix.karma_sparse import is_karmasparse
 
 from karma.core.utils.utils import coerce_to_tuple_and_check_all_strings
 from karma.core.curve import compute_mean_curve, gain_curve_from_prediction
 from karma.core.utils.array import is_binary
-from karma.learning.matrix_utils import to_scipy_sparse
 
-__all__ = ['regression_on_blocks', 'linear_regression_coefficients', 'sklearn_regression_model',
+__all__ = ['regression_on_blocks', 'linear_regression_coefficients',
            'create_meta_of_regression', 'create_summary_of_regression']
 
 
@@ -42,63 +38,6 @@ def linear_regression_coefficients(X, y, intercept=True, C=1e10, sample_weight=N
     lr.fit(X, y, sample_weight)
 
     return lr.predict(X), lr.intercept_, lr.coef_.T
-
-
-@regression_on_blocks
-def sklearn_regression_model(X, y, regression_class, classifier=False, **kwargs):
-    if 'max_features' in kwargs:
-        kwargs['max_features'] = min(X.shape[1], kwargs['max_features'])
-
-    if classifier:
-        y = y * 2 - 1
-
-    if is_karmasparse(X):
-        X = X.to_scipy_sparse(copy=False)
-
-    clf = regression_class(random_state=X.shape[0], **kwargs)
-    clf.fit(X, y)
-
-    y_hat = clf.predict_proba(X) if classifier else clf.predict(X)
-
-    return y_hat, clf
-
-
-def lightfm_regression_model(user_features, item_features, interactions, sample_weight=None, **kwargs):
-    # user_features and item_features must respect order of users and items from interactions matrix
-    user_features = to_scipy_sparse(user_features)
-    item_features = to_scipy_sparse(item_features)
-    interactions = to_scipy_sparse(interactions)
-
-    if sample_weight is not None:
-        sample_weight = to_scipy_sparse(sample_weight)
-
-    clf = LightFM(no_components=kwargs['no_components'],
-                  k=kwargs.get('k', 5),
-                  n=kwargs.get('n', 10),
-                  learning_schedule=kwargs.get('learning_schedule', 'adagrad'),
-                  loss=kwargs.get('loss', 'logistic'),
-                  learning_rate=kwargs.get('learning_rate', 0.05),
-                  rho=kwargs.get('rho', 0.95),
-                  epsilon=kwargs.get('epsilon', 1e-6),
-                  item_alpha=kwargs.get('item_alpha', 0.0),
-                  user_alpha=kwargs.get('user_alpha', 0.0),
-                  max_sampled=kwargs.get('max_sampled', 10),
-                  random_state=user_features.shape[0])
-    clf.fit(interactions,
-            user_features=user_features, item_features=item_features,
-            sample_weight=sample_weight,
-            epochs=kwargs.get('epochs', 1),
-            num_threads=kwargs.get('num_threads', 1),
-            verbose=kwargs.get('verbose', False))
-    # We need to replace user and items labels by integers
-    user_ids, item_ids = interactions.nonzero()
-    y = np.ravel(interactions[user_ids, item_ids])
-
-    y_linear = clf.predict(user_ids=user_ids, item_ids=item_ids,
-                           item_features=item_features, user_features=user_features,
-                           num_threads=kwargs.get('num_threads', 1))
-
-    return y, y_linear, clf
 
 
 def create_meta_of_regression(prediction, y, with_guess=True, test_curves=None, test_mses=None, name=None):
