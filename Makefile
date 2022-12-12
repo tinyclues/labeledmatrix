083c4387f9b64d7ed9181e4cdcf3b1ccd85f600a
Andrey Krivonogov <andrey.krivonogov@tinyclues.com>
2022-12-12 11:19:09 +0100

    Initial setup of docker and CI (#1)

---
+++ Makefile
@@ -0,0 +1,27 @@
+help: ## show help message.
+	@grep -E '^[a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | sort | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}'
+
+build:  # build dev docker image.
+	docker-compose -f docker-compose.dev.yml build labeledmatrix
+
+DOCKER_CMD := docker-compose -f docker-compose.dev.yml run --rm labeledmatrix
+
+shell: build
+	$(DOCKER_CMD) /bin/bash
+
+test: build
+	$(DOCKER_CMD) pytest --cov cyperf
+
+lint: build
+	$(DOCKER_CMD) pylint cyperf --rcfile=setup.cfg
+
+setup: build
+	$(DOCKER_CMD) python setup.py build_ext --inplace
+
+build_lock: # build docker image for pipenv lock
+	docker-compose -f docker-compose.dev.yml build lock
+
+DOCKER_LOCK_CMD := docker-compose -f docker-compose.dev.yml run --rm lock
+
+lock: build_lock
+	$(DOCKER_LOCK_CMD) pipenv lock --verbose
---
+++ version.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+
+# This script is used in setup.cfg and circleci.
+import logging
+from subprocess import check_output
+
+LOGGER = logging.getLogger(__name__)
+
+
+def _run(args):
+    return check_output(args).decode('utf8').strip()
+
+
+def is_git_repo():
+    cmd = ["git", "rev-parse", "--is-inside-work-tree"]
+    try:
+        output = _run(cmd)
+        LOGGER.info(f"is git repo cmd output: {output}")
+        if output == "true":
+            return True
+    except Exception as e:
+        LOGGER.exception(e)
+    return False
+
+
+def get_version():
+    LOGGER.info("Geting version using git.")
+    if not is_git_repo():
+        # In the production docker image git folder doesn't exist.
+        LOGGER.warn("We are not in a git folder, probably running in production, cannot return version.")
+        version_returned = 'unknown'
+        print(f"version=={version_returned}")
+        return version_returned
+    describe_cmd = ['git', 'describe', '--tags', '--always']
+    last_tag = _run(describe_cmd + ['--abbrev=0'])  # '1.0.14'
+    describe = _run(describe_cmd)  # '1.0.14-2-gfaa2442'  {tag}-{nb_commit_since_tag}-{hash}'
+    if describe == last_tag:
+        version_returned = last_tag.replace('v', '', 1)
+        LOGGER.info("version is the last tag, version==%s", version_returned)
+        print(f"version=={version_returned}")
+        return version_returned
+    short_hash = describe[len(last_tag) + 1:].split('-')[1]
+    version_returned = "{}.dev0+{}".format(last_tag.replace('v', '', 1), short_hash[1:])
+    LOGGER.info("version==%s", version_returned)
+    print(f"version=={version_returned}")
+    return version_returned
+
+
+__version__ = get_version() or "local"
+
+if __name__ == '__main__':
+    print(__version__)
